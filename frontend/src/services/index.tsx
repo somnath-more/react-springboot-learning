8dfd7720f2ae43ecaa0df73ca28a9b7f700c8dac
Somnath  More <somnath.more@zemosolabs.com>
2023-12-12 09:41:24 +0530

    create axios

--- frontend/src/services/index.tsx
+++ frontend/src/services/index.tsx
@@ -14,19 +14,22 @@ import axios from "axios";
 //     return res.data;
 // }
 
-export const FETCH_DATA=async () => {
-    const res=await axios.get("http://localhost:8080/store");
+const instance=axios.create({
+    baseURL:'http://localhost:8080',
+})
+export const FETCH_DATA = async () => {
+    const res = await instance.get("/store");
     return res.data;
-}
-export const POST_DATA=async (data:{name:string,manufacturer:string,price:number}) => {
-    const res=await axios.post("http://localhost:8080/store",data);
-    console.log("Savedd");
+  };
+  
+  export const POST_DATA = async (data: { name: string, manufacturer: string, price: number }) => {
+    const res = await instance.post("/store", data);
+    console.log("Saved");
     console.log(res.data);
-    
     return res.data;
-}
-
-export const  FETCH_WatchList_DATA=async () => {
-    const res=await axios.get("http://localhost:8080/store/watchlist");
+  };
+  
+  export const FETCH_WatchList_DATA = async () => {
+    const res = await instance.get("/store/watchlist");
     return res.data;
-}
+  };
